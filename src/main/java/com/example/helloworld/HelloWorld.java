3470f500271eba16149bd2deb1d0fdd21368aacf
Tom Pearson <tom_pearson@yahoo.com>
2021-09-30 10:33:34 -0700

    initial comment gethub-example-package

---
+++ src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+        public void printHelloWorld() {
+            System.out.println("This is a Hello World.");
+        }
+
+//    public class MyClass {
+        public static void main(String args[])
+        {
+            HelloWorld hello = new HelloWorld();
+            hello.printHelloWorld();
+        }
+//    }
+}
+
---
+++ src/test/java/com/example/helloworldTest/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.example.helloworldTest;
+
+public class HelloWorldTest {
+    public void printHelloWorldTest() {
+        System.out.println("This is a Hello World Test.");
+    }
+
+    //    public class MyClass {
+    public static void main(String args[])
+    {
+        HelloWorldTest hello = new HelloWorldTest();
+        hello.printHelloWorldTest();
+    }
+//    }
+}
